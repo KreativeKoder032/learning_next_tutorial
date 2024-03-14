15b2fd7466b3d6b365939bc47cfe29098c22b938
Niklas Koehler <niklas.koehler@knownwell.com>
2024-03-14 17:18:33 -0400

    Added some error handling

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -15,7 +15,7 @@ export default async function Page() {
     const numberOfCustomers = cardData.numberOfCustomers;
     return (
         <main>
-            <h1 className={`${lusitana.className} mb-4 text-xl md:text 2x1`}>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text 2xl`}>
                 Dashboard
             </h1>
             <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'>
